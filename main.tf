8e374f294754872ba4c0efa0514bf861b374820e
Jake Lundberg <grove.mountain.github@nym.hush.com>
2020-07-06 15:54:01 -0700

    dev: adding in gcp node for testing.

--- main.tf
+++ main.tf
@@ -41,3 +41,31 @@ resource "aws_iam_policy" "policy" {
 EOF
 
 }
+
+
+provider "google" {
+  credentials = "${var.gcp_credentials}"
+  project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
+}
+
+resource "google_compute_instance" "demo" {
+  name         = "${var.instance_name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.gcp_zone}"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+}
