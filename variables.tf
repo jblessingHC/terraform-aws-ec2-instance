b0a7c4a7a9a5910833980a7250782deba4ad29d6
Jake Lundberg <grove.mountain.github@nym.hush.com>
2020-07-06 16:13:18 -0700

    dev: updating variables

--- variables.tf
+++ variables.tf
@@ -43,3 +43,36 @@ variable "iam_policy_actions" {
   type = "list"
   default = [ "iam:List*" ]
 }
+
+variable "gcp_credentials" {
+  description = "GCP credentials needed by google provider"
+}
+
+variable "gcp_project" {
+  description = "GCP project name"
+}
+
+variable "gcp_region" {
+  description = "GCP region, e.g. us-east1"
+  default = "us-east1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone, e.g. us-east1-a"
+  default = "us-east1-b"
+}
+
+variable "machine_type" {
+  description = "GCP machine type"
+  default = "n1-standard-1"
+}
+
+variable "instance_name" {
+  description = "GCP instance name"
+  default = "demo"
+}
+
+variable "image" {
+  description = "image to build instance from"
+  default = "debian-9/debian-9-tf-1-10-v20180814"
+}
