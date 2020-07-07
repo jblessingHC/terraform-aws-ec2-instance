c05c0fcab357c34bf1097c206fd0cd4b0187fcff
Jake Lundberg <grove.mountain.github@nym.hush.com>
2020-07-07 13:56:21 -0700

    dev: less google

--- main.tf
+++ main.tf
@@ -41,30 +41,3 @@ resource "aws_iam_policy" "policy" {
 EOF
 
 }
-
-
-provider "google" {
-  project     = "${var.gcp_project}"
-  region      = "${var.gcp_region}"
-}
-
-resource "google_compute_instance" "demo" {
-  name         = "${var.instance_name}"
-  machine_type = "${var.machine_type}"
-  zone         = "${var.gcp_zone}"
-
-  boot_disk {
-    initialize_params {
-      image = "${var.image}"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral IP
-    }
-  }
-
-}
